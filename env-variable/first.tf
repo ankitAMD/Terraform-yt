233e19a76ee6935d07d44cca3bbe0cbddafd8360
Ankit Gupta <ankitAMD>
2024-12-26 18:25:30 +0530

    Lec20- Read environment variable in Terraform Configuration

---
+++ env-variable/first.tf
@@ -0,0 +1,19 @@
+variable age {
+    type = number
+  
+}
+
+variable username {
+    type = string
+  
+}
+output printname {
+    value = "Hello, ${var.username}! You are ${var.age} years old."
+}
+
+
+#terraform plan
+#terraform plan -var 'username=ankit' -var 'age=20'
+
+# export TF_VAR_username=Ankita
+#terraform plan
