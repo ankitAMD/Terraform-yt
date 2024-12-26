4793491fbec7d02c61bf197b329f727797345b5e
Ankit Gupta <ankitAMD>
2024-12-26 16:50:43 +0530

    Lecture 16 - Variable

--- list-variable/first.tf
+++ list-variable/first.tf
@@ -1,7 +1,16 @@
-variable users {
-         type = list
+output printfirst {
+    #value = "first user is ${var.users[0]}"
+    value = "${join("---->",var.users)}"
 }
 
-output printfirst {
-    value = "first user is ${var.users[0]}"
+output helloworldupper {
+    value = "${upper(var.users[0])}"
+}
+
+output helloworllower {
+    value = "${lower(var.users[1])}"
+}
+
+output helloworltitle {
+    value = "${title(var.users[0])}"
 }
---
+++ list-variable/variable.tf
@@ -0,0 +1,6 @@
+variable users {
+         type = list
+         default = ["gaurav","ANKIT","Supriya"]
+}
+
+#terraform plan -var 'users=["gaurav","ankit","Supriya"]
