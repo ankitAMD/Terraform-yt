d491a7f6d6da2e7d96af9995cb5f59b780cd1642
Ankit Gupta <ankitAMD>
2024-12-26 17:43:29 +0530

    Lec 17 map list dynamically

--- map-variable/variable.tf
+++ map-variable/variable.tf
@@ -15,3 +15,6 @@ output "userage" {
     value = "my name is ${var.username} and my age is  ${lookup(var.userage, "${var.username}")}"
   
 }
+
+#terraform plan -var 'username=gaurav'
+#terraform plan -var userage="{"gaurav"=20, "saurav"=19,"ankit"=23,"supriya"=22}" -var 'username=ankit'
