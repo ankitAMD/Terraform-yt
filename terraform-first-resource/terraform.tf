0c9e92a0209ad981d62679ae00ba31c4ffc2067a
Ankit Gupta <ankitAMD>
2024-12-26 19:09:10 +0530

    Lec22- Creating first Terraform Resource

---
+++ terraform-first-resource/terraform.tf
@@ -0,0 +1,28 @@
+provider "github" {
+  # Configuration options
+}
+
+resource "github_repository" "terraform-first-repo" {
+    name        = "terraform-first-repo"
+    description = "My first Terraform repository"
+    visibility = "public"
+    auto_init = true
+  
+}
+
+
+
+
+#provider "aws" {
+  # Configuration options
+#}
+
+#provider "ankit" {
+  # Configuration options
+#}
+
+#terraform providers
+#terraform init
+#terraform plan
+#terraform apply
+
